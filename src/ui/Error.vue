9cbf109b86c1447c571348fd131fda64a29b988a
Alberto Calvo <git@albertocalvo.com>
2020-01-20 12:09:05 +0100

    feat: clear error message on restricted URLs

--- src/ui/Error.vue
+++ src/ui/Error.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="simple">
     <h2>An error occurred</h2>
-    <p>
-      Please report it at
-      <a
-        href="https://github.com/acalvo/css-compat/issues"
-      >https://github.com/acalvo/css-compat/issues</a>
-    </p>
-    <pre>{{ error.value }}</pre>
+    <template v-if="error.code === 'E_PROTOCOLERROR'">
+      <p>This is a restricted webpage, for security reasons extensions cannot be executed in it.</p>
+    </template>
+    <template v-else>
+      <p>
+        Please report it at
+        <a
+          href="https://github.com/acalvo/css-compat/issues"
+        >https://github.com/acalvo/css-compat/issues</a>
+      </p>
+      <pre>{{ error.value }}</pre>
+    </template>
   </div>
 </template>
 
